306b4e282eb71aa713ed1b4995f13cfc9ba91281
Ryan Dufrene <crabbymonkey@users.noreply.github.com>
2023-09-16 00:57:56 -0400

    Update installscript.bash

--- installscript.bash
+++ installscript.bash
@@ -7,6 +7,10 @@ apt-get upgrade
 
 apt-get install vlc
 apt-get install git
+apt-get install python
+
+mkdir /var/git
+git clone https://github.com/crabbymonkey/onepiecetv /var/git/onepiecetv
 
 touch /etc/systemd/system/tvbutton.service
 sudo nano /etc/systemd/system/tvbutton.service
