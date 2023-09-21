a181aad984bf090a34512f5ab80d061575aa9d75
Ryan Dufrene <crabbymonkey@users.noreply.github.com>
2023-09-21 19:54:04 -0400

    Update installscript.sh

--- installscript.sh
+++ installscript.sh
@@ -13,8 +13,8 @@ mkdir /var/git
 git clone https://github.com/crabbymonkey/onepiecetv /var/git/onepiecetv
 chmod -R a+rw /var/git/onepiecetv
 
-cp /var/git/tvbutton.service /etc/systemd/system/tvbutton.service
-cp /var/git/tvplayer.service /etc/systemd/system/tvplayer.service
+cp /etc/systemd/system/tvbutton.service /var/git/tvbutton.service
+cp /etc/systemd/system/tvplayer.service /var/git/tvplayer.service
 
 systemctl enable tvbutton.service
 systemctl enable tvplayer.service
